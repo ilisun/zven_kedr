88b9c8bacfcaa7b474b92a0a8b0c022ed0b31bbc
ilisun <solaril@mail.ru>
2015-03-30 16:47:28 +0300

    30.03.15 1647

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # devise_for :users, :skip => :registrations
+  devise_for :users, :skip => :registrations
   resources :products
   resources :categories
 
